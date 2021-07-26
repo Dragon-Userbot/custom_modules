443367ebfa47064be41a80ee22338c8908b386aa
John <60581076+john-phonk@users.noreply.github.com>
2021-07-26 21:54:52 +0300

    Delete t4.py

--- t4.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
-from ..utils.utils import modules_help, prefix
-
-
-@Client.on_message(filters.command('t4', prefix) & filters.me)
-async def test2(client: Client, message: Message):
-    await message.edit('<code>TEST4</code>')
