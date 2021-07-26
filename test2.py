8c6cbb166178ff18667cbb13fd0b705d1d01c498
John <60581076+john-phonk@users.noreply.github.com>
2021-07-26 21:54:59 +0300

    Delete test2.py

--- test2.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
-from ..utils.utils import modules_help, prefix
-
-
-@Client.on_message(filters.command('t2', prefix) & filters.me)
-async def test2(client: Client, message: Message):
-    await message.edit('<code>TEST2</code>')
