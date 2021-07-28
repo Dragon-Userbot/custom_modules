4dbbdf39a2d69dd598ceef689aea0271b9c82061
John <60581076+john-phonk@users.noreply.github.com>
2021-07-28 19:45:50 +0300

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
-
-
-@Client.on_message(filters.command('t', '.') & filters.me)
-async def example_edit(client: Client, message: Message):
-    await message.edit('<code>TEST</code>')
