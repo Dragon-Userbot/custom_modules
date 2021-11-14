a1c9427756558f433cd0e35d3327981947270faf
Влад Приденьгах!!! <80100455+NavernoeDa@users.noreply.github.com>
2021-11-14 15:18:49 +0300

    update course_ru.py
    теперь это точно другой модуль

--- courses_ru.py
+++ courses_ru.py
@@ -4,9 +4,6 @@ from .utils.utils import modules_help, prefix
 import requests
 from bs4 import BeautifulSoup
 
-
-usd = 'https://ru.investing.com/currencies/usd-rub'
-eur = 'https://ru.investing.com/currencies/eur-rub'
 btc = 'https://ru.investing.com/crypto/bitcoin/btc-rub'
 
 headers = {
@@ -16,24 +13,25 @@ headers = {
 @Client.on_message(filters.command('course', prefix) & filters.me)
 async def convert(client: Client, message: Message):
     try:
-        await message.edit('<code>Подождите...</code>')
+        currency = message.command[1]
+        link = f'https://ru.investing.com/currencies/{currency}-rub'
+        await message.edit('<code>Data retrieval...</code>')
         if message.command[1] == 'usd':
             name = '1$'
-            link = usd
         elif message.command[1] == 'eur':
             name = '1€'
-            link = eur
         elif message.command[1] == 'btc':
             name = '1₿'
             link = btc
-        full_page = requests.get(link, headers=headers, timeout=1)
+        else:
+            name = currency
+        full_page = requests.get(link, headers=headers, timeout=3)
         soup = BeautifulSoup(full_page.content, 'html.parser')
         rub = soup.find('span', id='last_last')
-        await message.edit(f'<b>{name} стоит </b><code> {rub} </code><b> рублей</b>')
+        await message.edit(f'<b>{name} now is </b><code> {rub} </code><b> rub</b>')
     except:
-        await message.edit('<code>ERROR ЖОПЫ</code>')
+        await message.edit('<code>ERROR</code>')
 
 
-modules_help.update({'course': '''usd - Для перевода из доллара в рубль, 
-    eur - Для перевода из евро в рубль, btc - Для перевода из биткоина в рубль]\n[Не используйте чаще, чем 10 раз в минуту!''',
-    'course module': 'Course: usd, eur, btc'})
+modules_help.update({'course': '''usd/eur/btc and etc]\n[Dont use more than 10 times per minute''',
+    'course module': 'Course: usd, eur, btc and more'})
