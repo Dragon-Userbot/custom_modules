191ba9d6ccd5dbe66f85f72dc3e54ab97a776483
John <60581076+john-phonk@users.noreply.github.com>
2021-11-23 09:35:14 +0300

    Revert "Revert "create f.py""

---
+++ f.py
@@ -0,0 +1,37 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from ..utils.utils import modules_help, prefix
+from random import randint
+from os import system, remove
+
+try:
+    from wget import download        
+except:
+    system('pip install wget')
+    system('python main.py')
+
+
+def download_sticker(url):
+    download(url, 'f.webp')
+
+
+
+@Client.on_message(filters.command(['f'], prefix) & filters.me)
+async def random_stiker(client, message):
+    await message.delete()
+    random = randint(1, 63)
+    if random < 10:
+        index = f'00{random}'
+    else:
+        index = f'0{random}'
+    
+    sticker = f'https://www.chpic.su/_data/stickers/f/FforRespect/FforRespect_{index}.webp'
+    download_sticker(sticker)
+    await client.send_sticker(chat_id=message.chat.id, sticker='f.webp')
+    remove('f.webp')
+
+
+modules_help.update(
+    {'f': '''f - Send f to pay respect''',
+    'f module': 'f: f'}
+    )
