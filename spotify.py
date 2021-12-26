13b9e12182564e025db9166170b0ab8e9e42f37d
lav <me@lavn.ml>
2021-12-27 00:24:36 +0300

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -111,9 +111,6 @@ async def auth(client: Client, message: Message):
     else:
         await message.edit("⚠️Вы уже авторизованы")
 
-    else:
-        await message.edit("⚠️Вы уже авторизованы")
-
 
 @Client.on_message(filters.command("codeauth", prefix) & filters.me)
 async def codeauth(client: Client, message: Message):
