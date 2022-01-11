88ded86b285df787a9ab03ab3425ade2afe0f1d0
John <60581076+john-phonk@users.noreply.github.com>
2022-01-11 09:31:51 +0300

    Code styled to black

--- yt_preview.py
+++ yt_preview.py
@@ -9,11 +9,13 @@ from ..utils.utils import modules_help, prefix
 MAX_URL = "https://img.youtube.com/vi/{id}/maxresdefault.jpg"
 HQ_URL = "https://img.youtube.com/vi/{id}/hqdefault.jpg"
 
+
 def get_video_id(url):
     try:
         return urllib.parse.parse_qs(urllib.parse.urlparse(url).query)["v"][0]
     except Exception:
         return url.replace("&feature=share", "").split("/")[-1]
+
 
 @Client.on_message(filters.command("preview", prefix) & filters.me)
 async def preview(client, message):
@@ -22,13 +24,23 @@ async def preview(client, message):
             video_id = get_video_id(message.reply_to_message.text)
             video_id = MAX_URL.format(id=video_id)
             await message.edit("Upload Preview")
-            await client.send_photo(message.chat.id, video_id, caption=f"<a href='{video_id}'>Download Link</a>", parse_mode="HTML")
+            await client.send_photo(
+                message.chat.id,
+                video_id,
+                caption=f"<a href='{video_id}'>Download Link</a>",
+                parse_mode="HTML",
+            )
             await message.delete()
         elif len(message.command) == 2:
             video_id = get_video_id(message.command[1])
             video_id = MAX_URL.format(id=video_id)
             await message.edit("Upload Preview")
-            await client.send_photo(message.chat.id, video_id, caption=f"<a href='{video_id}'>Download Link</a>", parse_mode="HTML")
+            await client.send_photo(
+                message.chat.id,
+                video_id,
+                caption=f"<a href='{video_id}'>Download Link</a>",
+                parse_mode="HTML",
+            )
             await message.delete()
         elif len(message.command) == 3:
             video_id = get_video_id(message.command[1])
@@ -36,13 +48,22 @@ async def preview(client, message):
             view = message.command[2]
             await message.edit("Upload Preview")
             if view == "1":
-                await client.send_photo(message.chat.id, video_id, caption=f"<a href='{video_id}'>Download Link</a>", parse_mode="HTML")
+                await client.send_photo(
+                    message.chat.id,
+                    video_id,
+                    caption=f"<a href='{video_id}'>Download Link</a>",
+                    parse_mode="HTML",
+                )
                 await message.delete()
             elif view == "2":
-                await client.send_photo(message.chat.id, video_id, caption=f"Download Link - {video_id}")
+                await client.send_photo(
+                    message.chat.id, video_id, caption=f"Download Link - {video_id}"
+                )
                 await message.delete()
             elif view == "3":
-                await client.send_photo(message.chat.id, video_id, caption=f"{video_id}")
+                await client.send_photo(
+                    message.chat.id, video_id, caption=f"{video_id}"
+                )
                 await message.delete()
             elif view == "4":
                 await client.send_photo(message.chat.id, video_id)
@@ -50,10 +71,16 @@ async def preview(client, message):
             elif view == "5":
                 captionText = message.command[3:]
                 captionText = " ".join(captionText)
-                await client.send_photo(message.chat.id, video_id, caption=f"<a href='{video_id}'>{captionText}</a>", parse_mode="HTML")
+                await client.send_photo(
+                    message.chat.id,
+                    video_id,
+                    caption=f"<a href='{video_id}'>{captionText}</a>",
+                    parse_mode="HTML",
+                )
                 await message.delete()
     except:
         await message.edit(f"This <a href='{video_id}'>link</a> does not exist")
+
 
 modules_help.append(
     {
