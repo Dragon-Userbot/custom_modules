be1d87d096b8f61f818e43a88b3c291c9f87b448
TupaBan-Dev <78302942+TupaBan-Dev@users.noreply.github.com>
2022-01-11 18:37:00 +0300

    Global Update TiktTok.py

--- tiktok.py
+++ tiktok.py
@@ -1,34 +1,49 @@
-import asyncio
-
 from pyrogram import Client, filters
-from pyrogram.raw import functions
 from pyrogram.types import Message
 from ..utils.utils import modules_help, prefix
 
-@Client.on_message(filters.command("tt", prefix) & filters.me)
-async def tiktok(client: Client, message: Message):
-    try:
-        if message.reply_to_message:
-            link = message.reply_to_message.text
-        elif len(message.command) == 2:
-            link = message.command[1]
-        else:
-            return await message.edit(
-                "<i>Вы не указали ссылку, ознакомьтесь с документацией этого модуля</i>"
-            )
-        await message.edit("<i>Загрузка...</i>")
-        await client.send_message("@downloader_tiktok_bot", link)
-        await asyncio.sleep(3)
-        messages = await client.get_history("@downloader_tiktok_bot")
-        video = messages[0].video.file_id
-        await message.delete()
-        await client.send_video(message.chat.id, video)
-        await client.send(functions.messages.DeleteHistory(peer=await client.resolve_peer("@downloader_tiktok_bot"), max_id=0, revoke=True))
-    except AttributeError:
-        return await msg.edit(
-            "<i>Произошла ошибка при скачивании, попробуйте снова!</i>"
-        )
+async def download_video(video_url, name):
+  requestsVideo = requests.get(video_url, allow_redirects=True)
+  open(f"./downloads/video{name}.mp4", "wb").write(requestsVideo.content)
 
+async def sendVideo(message, client, video_url):
+  await message.delete()
+  chars = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+  create_hash = ""
+  for i in range(10):
+    create_hash += random.choice(chars)
+  startDownload = time.time()
+  snaptik(video_url).get_media()[0].download(f"./downloads/{create_hash}.mp4")
+  path = f"./downloads/{create_hash}.mp4"
+  endDownload = time.time() - startDownload
+  with open(f"./downloads/{create_hash}.mp4", "rb") as file:
+    await client.send_video(
+      chat_id = message.chat.id,
+      video=file,
+      caption=f"{round(endDownload, 4)} s"
+    )
+  os.remove(path)
+
+if not os.path.exists("downloads"):
+  os.makedirs("downloads")
+
+@Client.on_message(filters.command("tt", prefix) & filters.me)
+async def tiktok(client, message):
+  try:
+    if message.reply_to_message:
+      video_url = message.reply_to_message.text
+      await message.edit("Ожидайте")
+      await message.edit("Скачиваю видео")
+      await sendVideo(message, client, video_url)
+    elif len(message.command) > 1:
+      video_url = message.command[1]
+      await message.edit("Ожидайте")
+      await message.edit("Скачиваю видео")
+      await sendVideo(message, client, video_url)
+    else:
+      await message.edit("Реплайните сообщение с ссылкой, или укажите ссылку в аргементе")
+  except:
+    await message.edit("Ошибка скачки")
 
 modules_help.append(
     {
