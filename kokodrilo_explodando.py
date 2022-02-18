e180c3e9f21b5d83d2690336c998ac9c1f5e51f1
John <60581076+john-phonk@users.noreply.github.com>
2022-02-18 13:33:19 +0300

    kOkOdRiLo ExPlOrAdO

---
+++ kokodrilo_explodando.py
@@ -0,0 +1,22 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from utils.misc import modules_help, prefix
+import asyncio, random
+
+
+@Client.on_message(filters.command("kokodrilo", prefix) & filters.me)
+async def kokodrilo_explodando(_, message: Message):
+    amount = 1
+    if len(message.command) > 1:
+        amount = int(message.command[1])
+    for _ in range(amount):
+        await message.edit("🐊")
+        await asyncio.sleep(random.uniform(1, 2.5))
+        await message.edit("💥")
+        await asyncio.sleep(1.8)
+
+
+modules_help["kokodrilo_explodando"] = {
+    "kokodrilo [number of explosions]*": "<b>kOkOdRiLo ExPlOrAdO</b>",
+}
