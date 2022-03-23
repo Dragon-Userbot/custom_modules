6f8a37bd184eac452fc6882c557b4f53f887a688
AbhiTheModder <85984486+AbhiTheModder@users.noreply.github.com>
2022-03-23 11:27:57 +0530

    New module gmute & score (#39)
    * Create compressor.py
    
    * Delete compressor.py
    
    * Create mute.py
    
    * Create exceptions.py
    
    * Update exceptions.py
    
    * Rename mute.py to gban.py
    
    * Rename gban.py to gban_fun.py
    
    * Update gban_fun.py
    
    * Update gban_fun.py
    
    * Update gban_fun.py
    
    * Update gban_fun.py
    
    * Update gban_fun.py
    
    * Update gban_fun.py
    
    * Rename gban_fun.py to gmute.py
    
    * Update gmute.py
    
    * Update exceptions.py
    
    * Update gmute.py
    
    * Create score.py
    
    I'm big fan cricket but lazy on leaving tg so made but during writing this script one friday user gave me already written script so i made some changes to the code for working in dragon also 😁
    
    * Refactor gmute and score module
    
    Co-authored-by: Albert <63642425+iamnalinor@users.noreply.github.com>

---
+++ gmute.py
@@ -0,0 +1,64 @@
+from pyrogram import Client, filters, errors
+from pyrogram.types import Message
+from utils.misc import modules_help, prefix
+from utils.db import db
+
+
+def get_arg(message):
+    msg = message.text
+    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
+    split = msg[1:].replace("\n", " \n").split(" ")
+    if " ".join(split[1:]).strip() == "":
+        return ""
+    return " ".join(split[1:])
+
+
+@Client.on_message(filters.command("gmute", prefix) & filters.me)
+async def gmute(client, message):
+    if reply := message.reply_to_message:
+        user = reply.from_user.id
+    else:
+        user = get_arg(message)
+        if not user:
+            await message.edit("<b>Whom should I gmute?</b>")
+            return
+    get_user = await client.get_users(user)
+    gmuted_users = db.get("core.gmute", "gmuted_users", [])
+    gmuted_users.append(get_user.id)
+    db.set("custom.gmute", "gmuted_users", gmuted_users)
+    await message.edit(f"<b>Gmuted {get_user.first_name}, LOL!</b>")
+
+
+@Client.on_message(filters.command("ungmute", prefix) & filters.me)
+async def ungmute(client, message):
+    if reply := message.reply_to_message:
+        user = reply.from_user.id
+    else:
+        user = get_arg(message)
+        if not user:
+            await message.edit("<b>Whom should I ungmute?</b>")
+            return
+    get_user = await client.get_users(user)
+    gmuted_users = db.get("core.gmute", "gmuted_users", [])
+    try:
+        gmuted_users.remove(get_user.id)
+    except ValueError:
+        pass
+    db.set("custom.gmute", "gmuted_users", gmuted_users)
+    await message.edit(f"<b>Unmuted {get_user.first_name}, enjoy!</b>")
+
+
+@Client.on_message(filters.group & filters.incoming)
+async def check_and_del(_, message: Message):
+    if message.from_user.id in db.get("custom.gmute", "gmuted_users", []):
+        try:
+            await message.delete()
+        except errors.RPCError:
+            pass  # you don't have delete rights
+    message.continue_propagation()
+
+
+modules_help["gmute"] = {
+    "gmute": " global mute of user",
+    "ungmute": "unmute user from global ban",
+}
---
+++ score.py
@@ -0,0 +1,38 @@
+from utils.scripts import import_library
+from utils.misc import modules_help, prefix
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from bs4 import BeautifulSoup
+from typing import Union
+
+aiohttp = import_library("aiohttp")
+
+
+def get_text(message: Message) -> Union[str, None]:
+    """Extract Text From Commands"""
+    if message.text is None:
+        return
+    if " " not in message.text:
+        return
+    try:
+        return message.text.split(None, 1)[1]
+    except IndexError:
+        pass
+
+
+@Client.on_message(filters.command("score", prefix) & filters.me)
+async def score(_, message: Message):
+    score_page = "http://static.cricinfo.com/rss/livescores.xml"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(score_page) as resp:
+            page = await resp.text()
+    soup = BeautifulSoup(page, "html.parser")
+    result = soup.find_all("description")
+    sed = "".join(match.get_text() + "\n\n" for match in result)
+    await message.edit(
+        f"<b>Match information:</b><u> Credits Friday team</u>\n\n\n<code>{sed}</code>",
+        parse_mode="html",
+    )
+
+
+modules_help["score"] = {"score": "get live cricket scores"}
