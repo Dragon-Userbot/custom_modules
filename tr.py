c752c4faec33568fe1df3e6ea6bdab50ac00245c
Albert <63642425+iamnalinor@users.noreply.github.com>
2022-04-06 20:38:01 +0400

    Update tr.py

--- tr.py
+++ tr.py
@@ -4,24 +4,18 @@ from pyrogram.types import Message
 from pyrogram import Client, filters
 
 
-
-
-
 googletrans = import_library("googletrans","googletrans==4.0.0rc1")
 
 from googletrans import Translator
-
 trl = Translator()
 
 
-
-
-@Client.on_message(filters.command(["tr"], prefix) & filters.me)
+@Client.on_message(filters.command(["trans"], prefix) & filters.me)
 async def translate(_client, message):
     await message.edit_text("<b>Translating text...</b>")
     if message.reply_to_message and (message.reply_to_message.text or message.reply_to_message.caption):
         if len(message.text.split()) == 1:
-            await message.edit("Usage: Reply to a message, then <code>.tr [lang]*</code>")
+            await message.edit(f"<b>Usage: Reply to a message, then <code>{prefix}trans [lang]*</code></b>")
             return
         target = message.text.split()[1]
         if message.reply_to_message.text:
@@ -34,10 +28,10 @@ async def translate(_client, message):
         except ValueError as err:
             await message.edit("Error: <code>{}</code>".format(str(err)))
             return
-        await message.edit("<b>Translated from</b> <code>{}</code> to <code>{}</code>:\n\n<code>{}</code>".format(detectlang.lang, target, tekstr.text))
+        await message.edit("<b>Translated from <code>{}</code> to <code>{}</code></b>:\n\n<code>{}</code>".format(detectlang.lang, target, tekstr.text))
     else:
         if len(message.text.split()) <= 2:
-            await message.edit("Usage: <code>.tr [lang]* [text]*</code>")
+            await message.edit("<b>Usage: <code>.tr [lang]* [text]*</code></b>")
             return
         target = message.text.split(None, 2)[1]
         text = message.text.split(None, 2)[2]
@@ -47,9 +41,9 @@ async def translate(_client, message):
         except ValueError as err:
             await message.edit("Error: <code>{}</code>".format(str(err)))
             return
-        await message.edit("<b>Translated from</b> <code>{}</code> to <code>{}</code>:\n\n<code>{}</code>".format(detectlang.lang, target, tekstr.text))
+        await message.edit("<b>Translated from <code>{}</code> to <code>{}</code></b>:\n\n<code>{}</code>".format(detectlang.lang, target, tekstr.text))
         
-@Client.on_message(filters.command(["trdl"], prefix) & filters.me)
+@Client.on_message(filters.command(["transdl", "trdl"], prefix) & filters.me)
 async def translatedl(_client, message):
         dtarget = message.text.split(None, 2)[1]
         dtext = message.text.split(None, 2)[2]
@@ -62,6 +56,6 @@ async def translatedl(_client, message):
         await message.edit("{}".format(dtekstr.text))
 
 modules_help["translator"] = {
-    "tr": "reply to text with target language code or with language code send message",
-    "trdl": "Use when you want to chat with anyone in his language for example you chatting with russian guy and want to say hello do .trdl ru hello it'll edit your message with russian hello and user will be able to understand you and also he can't notice that you using translator"
+    "trans": "[lang]* [text/reply]* translate message",
+    "transdl": f"[lang]* [your text]* short variant of {prefix}trans"
 }
