c2881f7867b983b617e8b65caeeb14947f4170a1
vanilla <40017526+plasticsummer@users.noreply.github.com>
2022-05-03 00:27:25 +0600

    Update reactionspam.py

--- reactionspam.py
+++ reactionspam.py
@@ -5,101 +5,22 @@ from pyrogram.types import Message
 from utils.misc import modules_help, prefix
 from utils.scripts import format_exc
 
+emojis = ['👍', '👎', '❤️', '🔥', '🥰', '👏', '😁', '🤔', '🤯', '😱', '🤬', '😢', '🎉', '🤩', '🤮', '💩']
+
 @Client.on_message(filters.command("reactspam", prefix) & filters.me)
 async def reactspam(client: Client, message: Message):
 	amount = int(message.command[1])
 	reaction = " ".join(message.command[2:])
 	await message.edit(f"<b>One moment...</b>")
 	for i in range(amount):
-		if reaction == "👍":
+		if reaction in emojis:
 			try:
-        			await client.send_reaction(message.chat.id, message.message_id-i, "👍")
+				await client.send_reaction(message.chat.id, message.message_id-i, reaction)
 			except Exception as e:
 				return await message.edit(format_exc(e))
-		elif reaction == "👎":
-			try:
-        			await client.send_reaction(message.chat.id, message.message_id-i, "👎")
-			except Exception as e:
-				return await message.edit(format_exc(e))
-		elif reaction == "❤️":
-			try:
-        			await client.send_reaction(message.chat.id, message.message_id-i, "❤️")
-			except Exception as e:
-				return await message.edit(format_exc(e))
-		elif reaction == "🔥":
-			try:
-        			await client.send_reaction(message.chat.id, message.message_id-i, "🔥")
-			except Exception as e:
-				return await message.edit(format_exc(e))
-		elif reaction == "🥰":
-			try:
-        			await client.send_reaction(message.chat.id, message.message_id-i, "🥰")
-			except Exception as e:
-				return await message.edit(format_exc(e))
-		elif reaction == "👏":
-			try:
-        			await client.send_reaction(message.chat.id, message.message_id-i, "👏")
-			except Exception as e:
-				return await message.edit(format_exc(e))
-		elif reaction == "😁":
-			try:
-        			await client.send_reaction(message.chat.id, message.message_id-i, "😁")
-			except Exception as e:
-				return await message.edit(format_exc(e))
-		elif reaction == "🤔":
-			try:
-        			await client.send_reaction(message.chat.id, message.message_id-i, "🤔")
-			except Exception as e:
-				return await message.edit(format_exc(e))
-		elif reaction == "🤯":
-			try:
-        			await client.send_reaction(message.chat.id, message.message_id-i, "🤯")
-			except Exception as e:
-				return await message.edit(format_exc(e))
-		elif reaction == "😱":
-			try:
-        			await client.send_reaction(message.chat.id, message.message_id-i, "😱")
-			except Exception as e:
-				return await message.edit(format_exc(e))
-		elif reaction == "🤬":
-			try:
-        			await client.send_reaction(message.chat.id, message.message_id-i, "🤬")
-			except Exception as e:
-				return await message.edit(format_exc(e))
-		elif reaction == "😢":
-			try:
-        			await client.send_reaction(message.chat.id, message.message_id-i, "😢")
-			except Exception as e:
-				return await message.edit(format_exc(e))
-		elif reaction == "🎉":
-			try:
-        			await client.send_reaction(message.chat.id, message.message_id-i, "🎉")
-			except Exception as e:
-				return await message.edit(format_exc(e))
-		elif reaction == "🤩":
-			try:
-        			await client.send_reaction(message.chat.id, message.message_id-i, "🤩")
-
-			except Exception as e:
-				return await message.edit(format_exc(e))
-		elif reaction == "🤮":
-			try:
-				await client.send_reaction(message.chat.id, message.message_id-i, "🤮")
-			except Exception as e:
-				return await message.edit(format_exc(e))
-		elif reaction == "💩":
-			try:
-				await client.send_reaction(message.chat.id, message.message_id-i, "💩")
-			except Exception as e:
-				return await message.edit(format_exc(e))
-		else:
-			return await message.edit(f"<b>It has to be an emoji...</b>")
-
 	await message.edit(f"<b>Done!</b>")
 
 modules_help["reactionspam"] = {
 	"reactspam [amount]* [emoji]*": "spam reactions"
 
 }
-
-
