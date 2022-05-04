72e04ac589ccd8b170161046ec24eac5710b83b2
vanilla <40017526+plasticsummer@users.noreply.github.com>
2022-05-05 00:37:32 +0600

    Add files via upload

---
+++ fliptext.py
@@ -0,0 +1,109 @@
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.raw import functions
+from pyrogram.types import Message
+from utils.misc import modules_help, prefix
+from utils.scripts import format_exc
+
+REPLACEMENT_MAP = {
+    "a": "ɐ",
+    "b": "q",
+    "c": "ɔ",
+    "d": "p",
+    "e": "ǝ",
+    "f": "ɟ",
+    "g": "ƃ",
+    "h": "ɥ",
+    "i": "ᴉ",
+    "j": "ɾ",
+    "k": "ʞ",
+    "l": "l",
+    "m": "ɯ",
+    "n": "u",
+    "o": "o",
+    "p": "d",
+    "q": "b",
+    "r": "ɹ",
+    "s": "s",
+    "t": "ʇ",
+    "u": "n",
+    "v": "ʌ",
+    "w": "ʍ",
+    "x": "x",
+    "y": "ʎ",
+    "z": "z",
+    "A": "∀",
+    "B": "B",
+    "C": "Ɔ",
+    "D": "D",
+    "E": "Ǝ",
+    "F": "Ⅎ",
+    "G": "פ",
+    "H": "H",
+    "I": "I",
+    "J": "ſ",
+    "K": "K",
+    "L": "˥",
+    "M": "W",
+    "N": "N",
+    "O": "O",
+    "P": "Ԁ",
+    "Q": "Q",
+    "R": "R",
+    "S": "S",
+    "T": "┴",
+    "U": "∩",
+    "V": "Λ",
+    "W": "M",
+    "X": "X",
+    "Y": "⅄",
+    "Z": "Z",
+    "0": "0",
+    "1": "Ɩ",
+    "2": "ᄅ",
+    "3": "Ɛ",
+    "4": "ㄣ",
+    "5": "ϛ",
+    "6": "9",
+    "7": "ㄥ",
+    "8": "8",
+    "9": "6",
+    ",": "'",
+    ".": "˙",
+    "?": "¿",
+    "!": "¡",
+    '"': ",,",
+    "'": ",",
+    "(": ")",
+    ")": "(",
+    "[": "]",
+    "]": "[",
+    "{": "}",
+    "}": "{",
+    "<": ">",
+    ">": "<",
+    "&": "⅋",
+    "_": "‾",
+}
+
+
+@Client.on_message(filters.command("flip", prefix) & filters.me)
+async def flip(client: Client, message: Message):
+	text = message.text.split("flip", maxsplit=1)[1]
+	final_str = ""
+	for char in text:
+		if char in REPLACEMENT_MAP.keys():
+			new_char = REPLACEMENT_MAP[char]
+		else:
+			new_char = char
+		final_str += new_char
+	if text != final_str:
+		await message.edit(final_str)
+	else:
+		await message.edit(text)
+
+
+modules_help["fliptext"] = {
+	"flip [amount]*": "flip text upside down"
+
+}
