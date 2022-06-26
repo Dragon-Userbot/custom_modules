6b2c6b4883a65ba97ac3f2f3d3987ebc4dfae5ea
Albert <63642425+iamnalinor@users.noreply.github.com>
2022-06-26 21:06:02 +0400

    fuck sync functions

--- haste.py
+++ haste.py
@@ -9,19 +9,19 @@ BASE = "https://hastebin.com"
 
 @Client.on_message(filters.command("haste", prefix) & filters.me)
 @with_reply
-def haste(client: Client, message: Message):
+async def haste(client: Client, message: Message):
     reply = message.reply_to_message
 
     if reply.text is None:
         return
 
-    message.delete()
+    await message.delete()
 
     result = requests.post(
         "{}/documents".format(BASE), data=reply.text.encode("UTF-8")
     ).json()
 
-    message.reply(
+    await message.reply(
         "{}/{}.py".format(BASE, result["key"]), reply_to_message_id=reply.message_id
     )
 
